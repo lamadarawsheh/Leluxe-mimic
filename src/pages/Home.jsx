f04516295dd75d6d9776e3d81666409f14fa8a0f
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2024-09-29 00:13:26 +0300

    fix : fix order-now  button path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -125,9 +125,12 @@ const Home = () => {
                 </p>
 
                 <div className="hero__btns d-flex align-items-center gap-5 mt-4">
-                  <button className="order__btn d-flex align-items-center justify-content-between">
+                <Link to="/cart">
+                <button className="order__btn d-flex align-items-center justify-content-between">
                     Order now <i className="ri-arrow-right-s-line"></i>
                   </button>
+                </Link>
+                
 
                   <button className="all__foods-btn">
                     <Link to="/all">See all Categories</Link>
